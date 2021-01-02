4bc1370b4e386af60eb8de57adfaa7e13bf55ca4
Jamtech Dev <jamtechba@gmail.com>
2021-01-02 13:19:11 +0530

    Readme, Gitignore

---
+++ README.md
@@ -0,0 +1,18 @@
+## React Python Sample
+
+This project contains sample code from Python (Django Rest Framework) and JS (React) projects.
+
+### Python
+###### Django Rest Framework
+
+- The view file (`view.py`) contains business logic of a Django project made for providing data visualization inside a 
+Customer Experience Software for Surveys
+- The sample file consists mainly of helper functions used for calculations and relevant operations and some Viewsets for 
+returning data to the endpoints.
+
+### JavaScript
+###### React
+
+- The React sample (`Profile.js`) is from a React project serving the frontend of the same CX app
+- The sample code lets a User see and edit his profile
+- APIs and services used on the page are connected to the aforementioned Python project.
--- view.py
+++ view.py
@@ -1,8 +1,165 @@
+import random
+import re
+import string
+from datetime import datetime, timedelta
+
+from nltk import word_tokenize, pos_tag, WordNetLemmatizer
+from nltk.corpus import stopwords
+
+
+def make_filters(time_frame_filter: str = None, country_filter: str = None):
+    """
+    Creates and returns filters
+    
+    :param time_frame_filter: filter param
+    :param country_filter: filter param
+    :return: filters
+    """
+    filters = []
+    if time_frame_filter:
+        filter_type = 'time_frame'
+        filter_data = time_frame_filter.split('_')
+        if len(filter_data) == 3:
+            if filter_data[2] == 'hours':
+                delta = timedelta(hours=int(filter_data[1]))
+                filters.append(
+                    {
+                        'type': filter_type,
+                        'gte': datetime.now() - delta,
+                        'lte': datetime.now(),
+                    }
+                )
+            elif filter_data[2] == 'days':
+                delta = timedelta(days=int(filter_data[1]))
+                filters.append(
+                    {
+                        'type': filter_type,
+                        'gte': datetime.now() - delta,
+                        'lte': datetime.now(),
+                    }
+                )
+        elif len(filter_data) == 2:
+            if time_frame_filter == 'past_year':
+                delta = timedelta(days=365)
+                filters.append(
+                    {
+                        'type': filter_type,
+                        'gte': datetime.now() - delta,
+                        'lte': datetime.now(),
+                    }
+                )
+            elif time_frame_filter == 'year_to_date':
+                current_year = datetime.today().year
+                filters.append(
+                    {
+                        'type': filter_type,
+                        'gte': datetime(current_year, 1, 1),
+                        'lte': datetime.now()
+                    }
+                )
+            elif time_frame_filter == 'this_quarter':
+                today = datetime.today()
+                current_quarter = ((today.month - 1) // 3) + 1
+
+                quarter_month = None
+                if current_quarter == 1:
+                    quarter_month = 1
+                elif current_quarter == 2:
+                    quarter_month = 4
+                elif current_quarter == 3:
+                    quarter_month = 7
+                elif current_quarter == 4:
+                    quarter_month = 10
+
+                filters.append(
+                    {
+                        'type': filter_type,
+                        'gte': datetime(today.year, quarter_month, 1),
+                        'lte': datetime.now(),
+                    }
+                )
+            elif time_frame_filter == 'this_month':
+                today = datetime.today()
+                filters.append(
+                    {
+                        'type': filter_type,
+                        'gte': datetime(today.year, today.month, 1),
+                        'lte': datetime.now(),
+                    }
+                )
+            elif time_frame_filter == 'this_week':
+                today = datetime.today()
+                delta = timedelta(days=today.weekday())
+                filters.append(
+                    {
+                        'type': filter_type,
+                        'gte': datetime.now() - delta,
+                        'lte': datetime.now(),
+                    }
+                )
+
+    if country_filter:
+        filter_type = 'countries'
+        filter_data = country_filter.split(',')
+        filters.append(
+            {
+                'type': filter_type,
+                'countries': filter_data
+            }
+        )
+
+    return filters
+
+
+def without_keys(d, keys):
+    """
+    Returns the provided dictionary without specified keys
+
+    :param d: dictionary
+    :param keys: keys to remove
+    :return: dictionary after removing provided keys
+    """
+    return {x: d[x] for x in d if x not in keys}
+
+
+def remove_noise(statement):
+    """
+    Removes stop words and symbols
+
+    :param statement:
+    :return:
+    """
+    cleaned_tokens = []
+    stop_words = stopwords.words('english')
+    tokens = word_tokenize(statement)
+    for token, tag in pos_tag(tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*(),]|'
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
+        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+
+        if tag.startswith('NN'):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        # noinspection SpellCheckingInspection
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 2 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+
+        random.shuffle(cleaned_tokens)
+    return cleaned_tokens
+
+
 def get_nps_segment_data(pro_detract_count):
     """
     Returns NPS Segment data for a Survey / Company
 
-    :param survey: pro_detract_count
+    :param pro_detract_count: dict
     :return: nps_segment_data
     """
     nps_segment_data = {
@@ -40,6 +197,7 @@ def get_nps_segment_data(pro_detract_count):
 
     return nps_segment_data
 
+
 def get_nps_trend(company=None, survey=None):
     """
     Returns NPS Trend data for a Survey / Company
@@ -64,6 +222,7 @@ def get_nps_trend(company=None, survey=None):
         plotted_data[date_time] = change.nps
 
     return plotted_data
+
 
 def get_nps_volume_trend(survey):
     """
@@ -123,7 +282,7 @@ def get_nps_volume_trend(survey):
         }
     ]
     return series
-    
+
 
 def calculate_nps(promoters, detractors, respondents):
     """
@@ -139,7 +298,7 @@ def calculate_nps(promoters, detractors, respondents):
     detractors_percent = (detractors / respondents) * 100
     nps = promoters_percent - detractors_percent
     return int(round(nps))
-    
+
 
 def get_grade_sum(response_score):
     """
@@ -156,7 +315,23 @@ def get_grade_sum(response_score):
                 grade_sum += grade * score
             grade += 1
     return grade_sum
-    
+
+
+class IsAuthenticatedOrWriteOnly(BasePermission):
+    """
+    The request is authenticated as a user, or is a write-only request.
+    """
+
+    def has_permission(self, request, view):
+        # noinspection PyPep8Naming
+        WRITE_METHODS = ["POST", ]
+
+        return (
+                request.method in WRITE_METHODS or
+                request.user and
+                request.user.is_authenticated
+        )
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -172,10 +347,10 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response({"message": "Registration was successful!"}, 
+        return Response({"message": "Registration was successful!"},
                         status=status.HTTP_201_CREATED, headers=headers)
-                        
-                        
+
+
 class AuthProfileViewSet(viewsets.ModelViewSet):
     """
     API endpoint for logged in user profiles.
@@ -188,8 +363,8 @@ class AuthProfileViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return self.queryset.filter(user=self.request.user)
         return self.queryset
-        
-        
+
+
 class CompanyViewSet(viewsets.ModelViewSet):
     """
     API endpoint for companies
@@ -203,6 +378,5 @@ class CompanyViewSet(viewsets.ModelViewSet):
         :param serializer:
         :return:
         """
-        sharing_pin_rand = randint(10000, 99999)
+        sharing_pin_rand = random.randint(10000, 99999)
         serializer.save(sharing_pin=sharing_pin_rand)
-
